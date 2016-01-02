8773b522855c74a700ad25958b5af9f425dc678f
Eric Dauenhauer <eric_yd@yahoo.com>
2016-01-02 12:11:47 -0800

    updated comments

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 /* If this script isn't working right, then cd into the root directory and run
     npm install express
-    
-express seems to be the package that allows this to run properly.
+
 Then navigate to http://localhost:8080/<filename> e.g. http://localhost:8080/index.html
 */
 
 var express = require('express'),
     app = express();
+process.stdout.write("hello: ");
 
 app.use(express.static(__dirname + ''));
 
-app.listen(8080)
+app.listen(8080);
