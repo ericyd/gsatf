ab9579313d5a14ff512e62b1c58123a73bf28750
Eric Dauenhauer <eric_yd@yahoo.com>
2016-01-03 09:39:53 -0800

    fixed bug in build

--- gulpfile.js
+++ gulpfile.js
@@ -10,36 +10,40 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     dest = 'dist';
     
-var watcher = gulp.watch(['src/**/*'], ['js', 'css', 'html']);
-watcher.on('change', function (event) {
-   console.log('Event type: ' + event.type); // added, changed, or deleted
-   console.log('Event path: ' + event.path); // The path of the modified file
-});
+
 
 gulp.task('default', function() {
     return; 
+});
+
+gulp.task('watch', function(){
+    var watcher = gulp.watch(['src/**/*'], ['js', 'css', 'html']);
+    watcher.on('change', function (event) {
+        console.log('Event type: ' + event.type); // added, changed, or deleted
+        console.log('Event path: ' + event.path); // The path of the modified file
+    });
 });
 
 
 // gulp tasks can be called with `gulp <taskName>` where <taskName> is the first argument of gulp.task    
 gulp.task('js', function() {
     // define what the task 'minify' actually does
-    gulp.src(['src/js/**/*.min.js', 'src/js/assets/sprintf.js'], { base: 'src' })
+    gulp.src('src/js/assets/*.js', { base: 'src' })
         .pipe(changed(dest))
         .pipe(gulp.dest(dest));
     // read for more detail http://www.smashingmagazine.com/2014/06/building-with-gulp/
-    return gulp.src(['src/js/**/*.js', '!src/js/**/*.min.js', '!src/js/assets/sprintf.js'], { base: 'src' })
+    return gulp.src(['src/js/controllers/*.js', 'src/js/directives/*.js', 'src/js/factories/*.js'])
         // only continues with files that have changed
-        .pipe(changed(dest))
+        //.pipe(changed(dest))
         // looks for errors and prints them
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         // minifies the contents with uglify
         .pipe(uglify())
         // concatenates all js files into one main file named 'main.js'
-        //.pipe(concat('main.js'))
+        .pipe(concat('app.js'))
         // pipes the output to the destination folder
-        .pipe(gulp.dest(dest));
+        .pipe(gulp.dest('dist/js'));
     
 });
 
@@ -47,6 +51,9 @@ gulp.task('css', function() {
     // define what the task 'minify' actually does
     // move unchaged files to final location
     gulp.src('src/css/**/*.min.css', { base: 'src' })
+        .pipe(changed(dest))
+        .pipe(gulp.dest(dest));
+    gulp.src(['src/css/**/*.css', '!src/css/**/*.min.css'], { base: 'src' })
         .pipe(changed(dest))
         .pipe(gulp.dest(dest));
     // read for more detail http://www.smashingmagazine.com/2014/06/building-with-gulp/
@@ -72,27 +79,4 @@ gulp.task('html', function() {
         
 });
 
-gulp.task('build', function() {
-    gulp.src('src/**/*.html', { base: 'src' })
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-            removeCommentsFromCDATA: true,
-            conservativeCollapse: true,
-        }))
-        .pipe(gulp.dest(dest));
-    gulp.src('src/css/**/*.min.css', { base: 'src' })
-        .pipe(gulp.dest(dest));
-    gulp.src(['src/css/**/*.scss', '!src/css/**/*.min.css'], { base: 'src' })
-        .pipe(sass())
-        .pipe(cssmin())
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest(dest));
-    gulp.src(['src/js/**/*.min.js', 'src/js/sprintf.js'], { base: 'src' })
-        .pipe(gulp.dest(dest));
-    gulp.src(['src/js/**/*.js', '!src/js/**/*.min.js', '!src/js/sprintf.js'], { base: 'src' })
-        .pipe(uglify())
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest(dest));
-    return;
-});
+gulp.task('build', ['js', 'css', 'html']);
