6fadde23fd3352b052c019c8665708cf079c8974
Eric Dauenhauer <eric_yd@yahoo.com>
2016-01-04 18:34:12 -0800

    finished parser for headers, title rows, and bottom row

--- src/js/factories/factory.js
+++ src/js/factories/factory.js
@@ -31,6 +31,7 @@ gsatf.factory("gsatfFactory", function() {
         right: "right",
         tdSubhead: "tdSubhead",
         tdBody: "tdBody",
+        borderTop: "borderTop",
         nCols: 0, // updated in script
         nRows: 1
     };
@@ -153,12 +154,16 @@ gsatf.factory("gsatfFactory", function() {
         return tableHtml;
     } 
 
+
+
     function getTitle() {
         // set up html for first row
         var head = sprintf("<tr><td id='0000' colspan='%(nCols)i' class='%(tdTitle)s %(center)s %(trTitle)s'><span editable-text='table.title'>", css);
         var tail = "</span></td></tr>";
         return head + "{{ table.title }}" + tail;
     }
+
+
 
     function getHeaders() {
         
@@ -221,8 +226,10 @@ gsatf.factory("gsatfFactory", function() {
         
     }
     
+    
+    
     function getBody() {
-        var data = table.body;
+        
         var bodyHtml = "";
         
         // iterate through remaining rows, and process
@@ -230,16 +237,14 @@ gsatf.factory("gsatfFactory", function() {
             var row = table.body[i];
             bodyHtml += "<tr>";
             
-            // if its not the first or last line of the table
-            if ( i != table.body.length - 1 ) {
-
-                // if there's only a single element in the row
-                if (table.body[i].length == 1) {
-                    var id = i < 10 ? "0" + i.toString() + "00" : i.toString();
-                    bodyHtml += sprintf("<td id='%s' colspan='%s' class='%s %s'><span editable-text='table.body[%s][0]'>", id, css.nCols, css.tdSubhead, i);
-                    bodyHtml += sprintf("{{ table.body[%s][0] }}</span></td></tr>", i);
-                    continue;
-                } 
+            // if there's only a single element in the row
+            if (table.body[i].length == 1) {
+                
+                var id = i < 10 ? "0" + i.toString() + "00" : i.toString() + "00";
+                bodyHtml += sprintf("<td id='%s' colspan=%s class='%s'><span editable-text='table.body[%s][0]'>", id, css.nCols, css.tdSubhead, i);
+                bodyHtml += sprintf("{{ table.body[%s][0] }}</span></td></tr>", i);
+                
+            } else {
                 
                 // loop through and process 
                 for (var j = 0; j < table.body[i].length; j++) {
@@ -247,14 +252,8 @@ gsatf.factory("gsatfFactory", function() {
                     var J = j < 10 ? "0"+j.toString() : j.toString();
                     bodyHtml += sprintf("<td id='%s' class='%s %s'><span editable-text='table.body[%i][%i]'>%s</span></td>", I+J, css.tdBody, css.center, i, j, "{{ table.body[" + i + "][" + j + "] }}");
                 }
-                
-            } else {
-                for (var j = 0; j < table.body[i].length; j++) {
-                    var I = i < 10 ? "0"+i.toString() : i.toString();
-                    var J = j < 10 ? "0"+j.toString() : j.toString();
-                    bodyHtml += sprintf("<td id='%s' class='%s %s %s'><span editable-text='table.body[%i][%i]'>%s</span></td>", I+J, css.tdBody, css.center, css.trBreak, i, j, "{{ table.body[" + i + "][" + j + "] }}");
-                }
             }
+                
 
             bodyHtml += "</tr>";
         }
@@ -262,8 +261,10 @@ gsatf.factory("gsatfFactory", function() {
         return bodyHtml;
     }
     
+    
+    
     function getFootnotes() {
-        var html = "";
+        var html = sprintf("<tr><td colspan=%i class='%s'></td></tr>", css.nCols, css.borderTop);
         
         return html;
     }
@@ -272,109 +273,3 @@ gsatf.factory("gsatfFactory", function() {
     
     return factory;
 });
-
-
-
-
-
-
-
-
-
-
-/*
-factory.parseData = function(information) {
-
-        //del = cellDelimiter;
-        
-        // Declare other variables
-        //var data = angular.element(document.getElementById('textbox')).value; //$("#textBox").val();
-        var data = information;
-        
-        //var table = '<table class="table">';
-        //var table = '<table cellspacing=0 border=0>';
-        tableHtml = '<table cellspacing=0 border=0>';
-        var isFilled = function(x) { return x !== ""; };
-        var isNotComment = function(x) { return (x[0] != "#" && x.slice(0,2) != "//"); };
-        var reFootnoteInc = /\*\d+/; //inclusive
-        var reFoodnoteExc = /\*(\d+)/; //exclusive
-        
-        // split each row into an array
-        data = data.split(/\n/);
-        
-        // clear empty rows at end
-        //while (data[data.length - 1] == "") { data.pop(); };
-        data = data.filter(isFilled).filter(isNotComment);
-        
-        // split rows into cells & strip empty cells from "title rows" (rows where only the first cell is populated)
-        for (i = 0; i < data.length; i++) { 
-            data[i] = data[i].split(/\t/); 
-            data_filter = data[i].filter(isFilled);
-            // if there's only one non-empty cell AND its the first cell in the array
-            if (data_filter.length == 1 && data_filter[0] == data[i][0]) {
-                data[i] = data_filter;
-            }
-        }
-        
-        // find max row length (= num of cols) - note: slice() effectively creates a copy of data, so the sorting doesn't affect the table
-        css.nCols = data.slice().sort(function (a, b) { return b.length - a.length; })[0].length;
-
-        // Create Title (or empty placeholder)
-        // one element in first row indicates that its a built-in title
-        if (data[0].length == 1) {
-            tableHtml += getTitle(data.shift()[0], css);
-        // more than one element indicates that its the headers; less than one is likely an error
-        } else {
-            tableHtml += getTitle("Title 1.", css); 
-        }
-        
-        
-        
-        // Get headers
-        tableHtml += getHeaders(data.shift(), css);
-        data.unshift("", "");
-        
-        // iterate through remaining rows, and process
-        for (var i = 2; i < data.length; i++) {
-            var row = data[i];
-            tableHtml += "<tr>";
-            
-            // if its not the first or last line of the table...
-            if ( i != data.length - 1 ) {
-                
-                
-                if (row.length == 1) {
-                    var id = i < 10 ? "0" + i.toString() + "00" : i.toString();
-                    tableHtml += sprintf("<td id='%s' colspan='%s' class='%s %s'>", id, css.nCols, css.tdSubhead);
-                    tableHtml += row[0] + "</td></tr>";
-                    continue;
-                }
-                
-                for (var j = 0; j < row.length; j++) {
-                    var I = i < 10 ? "0"+i.toString() : i.toString();
-                    var J = j < 10 ? "0"+j.toString() : j.toString();
-                    tableHtml += sprintf("<td id='%s' class='%s'>%s</td>", I+J, css.tdBody, row[j]);
-                }
-                
-            } else {
-                for (var j = 0; j < row.length; j++) {
-                    var I = i < 10 ? "0"+i.toString() : i.toString();
-                    var J = j < 10 ? "0"+j.toString() : j.toString();
-                    tableHtml += sprintf("<td id='%s' class='%s %s'>%s</td>", I+J, css.tdBody, css.trBreak, row[j]);
-                }
-            }
-            
-            
-            
-    
-            tableHtml += "</tr>";
-        }
-        
-        tableHtml += "</table>";
-    
-        return table;
-
-
-
-    }; // function parseData
-    */
