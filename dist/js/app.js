b7d9f59153cb424ddb814d9206964a3cdec89046
Eric Dauenhauer <eric_yd@yahoo.com>
2016-01-04 18:43:35 -0800

    first functional distribution

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,394 @@
-!function t(e,r,n){function l(i,a){if(!r[i]){if(!e[i]){var s="function"==typeof require&&require;if(!a&&s)return s(i,!0);if(o)return o(i,!0);var d=new Error("Cannot find module '"+i+"'");throw d.code="MODULE_NOT_FOUND",d}var c=r[i]={exports:{}};e[i][0].call(c.exports,function(t){var r=e[i][1][t];return l(r?r:t)},c,c.exports,t,e,r,n)}return r[i].exports}for(var o="function"==typeof require&&require,i=0;i<n.length;i++)l(n[i]);return l}({1:[function(t,e,r){var n=angular.module("gsaTableFormatter",["ngRoute","xeditable"]);n.controller("SimpleController",["$scope","gsatfFactory","$sce","$location",function(t,e,r,n){t.table=e.getTable(),t.tableHtml=e.buildHtml(),t.click=function(){t.table=e.extractData(t.textarea),t.tableHtml=e.buildHtml(),n.path("view2")},t.close=function(){$("#submitDataBtn").popover("hide")},t.user={name:r.trustAsHtml("eric<br>dauenhauer")}}]),n.config(["$routeProvider",function(t){t.when("/",{templateUrl:"pages/view1.html",controller:"SimpleController"}).when("/view2",{templateUrl:"pages/view2.html",controller:"SimpleController"}).otherwise({redirectTo:"/"})}])},{}],2:[function(t,e,r){gsatf.directive("customPopover",["$compile",function(t){return{restrict:"A",link:function(e,r,n){function l(r){var n={copyPaste:$("#copy-paste_popover_content").html(),upload:$("#upload_popover_content").html(),whichOne:$("#which-one_popover_content").html()};return t(n[r])(e)}$(r).popover({trigger:n.popoverTrigger,html:!0,content:l(n.popoverHtml),placement:n.popoverPlacement})}}}]),gsatf.directive("dynamic",["$compile","$sce",function(t,e){return{restrict:"A",replace:!0,link:function(e,r,n){e.$watch(n.dynamic,function(n){r.html(n),t(r.contents())(e)})}}}])},{}],3:[function(t,e,r){gsatf.factory("gsatfFactory",function(){function t(){var t=sprintf("<tr><td id='0000' colspan='%(nCols)i' class='%(tdTitle)s %(tdAll)s %(trTitle)s'><span editable-text='table.title'>",a),e="</span></td></tr>";return t+"{{ table.title }}"+e}function e(){for(var t=/\(.*\)/,e="<tr>",r=0;r<o.headers.length;r++){var n=10>r?"010"+r.toString():"01"+r.toString();t.test(o.headers[r])&&(console.log(t),console.log(o.headers[r].match(t)[0]),console.log(o.headers[r]),o.headers[r]=o.headers[r].replace(t,"<br />"+o.headers[r].match(t)[0]),console.log(o.headers[r])),e+=sprintf("<td id='%s' class='%s %s'>",n,a.tdCenter,a.trBreak),e+=sprintf("<span editable-text='table.headers[%i]'>{{ table.headers[%i] }}</span></td>",r,r)}return e+"</tr>"}function r(){for(var t=(o.body,""),e=0;e<o.body.length;e++){o.body[e];if(t+="<tr>",e!=o.body.length-1){if(1==o.body[e].length){var r=10>e?"0"+e.toString()+"00":e.toString();t+=sprintf("<td id='%s' colspan='%s' class='%s %s'><span editable-text='table.body[%i][0]'>",r,a.nCols,a.tdSubhead,e),t+="{{ table.body["+e+"][0] }}</span></td></tr>";continue}for(var n=0;n<o.body[e].length;n++){var l=10>e?"0"+e.toString():e.toString(),i=10>n?"0"+n.toString():n.toString();t+=sprintf("<td id='%s' class='%s'><span editable-text='table.body[%i][%i]'>%s</span></td>",l+i,a.tdAll,e,n,"{{ table.body["+e+"]["+n+"] }}")}}else for(var n=0;n<o.body[e].length;n++){var l=10>e?"0"+e.toString():e.toString(),i=10>n?"0"+n.toString():n.toString();t+=sprintf("<td id='%s' class='%s %s'><span editable-text='table.body[%i][%i]'>%s</span></td>",l+i,a.tdAll,a.trBreak,e,n,"{{ table.body["+e+"]["+n+"] }}")}t+="</tr>"}return t}function n(){var t="";return t}var l={},o={title:"",headers:[],body:[],footnotes:[]},i="",a={trTitle:"trTitle",trBreak:"trBreak",tdTitle:"tdTitle",tdCenter:"tdCenter",tdSubhead:"tdSubhead",tdAll:"tdAll",nCols:0,nRows:0};return l.getTable=function(){return o},l.extractData=function(t){var e=function(t){return""!==t},r=function(t){return"#"!=t[0]&&"//"!=t.slice(0,2)};for(t=t.split(/\n/),t=t.filter(e).filter(r),n=0;n<t.length;n++)t[n]=t[n].split(/\t/),data_filter=t[n].filter(e),1==data_filter.length&&data_filter[0]==t[n][0]&&(t[n]=data_filter);a.nCols=t.slice().sort(function(t,e){return e.length-t.length})[0].length,1==t[0].length?o.title=t.shift()[0]:o.title+="Title 1.",o.headers=t.shift();for(var n=0;n<t.length;n++)o.body.push(t[n])},l.parseData=function(r){var n=r;i="<table cellspacing=0 border=0>";var l=function(t){return""!==t},s=function(t){return"#"!=t[0]&&"//"!=t.slice(0,2)};for(n=n.split(/\n/),n=n.filter(l).filter(s),d=0;d<n.length;d++)n[d]=n[d].split(/\t/),data_filter=n[d].filter(l),1==data_filter.length&&data_filter[0]==n[d][0]&&(n[d]=data_filter);a.nCols=n.slice().sort(function(t,e){return e.length-t.length})[0].length,i+=1==n[0].length?t(n.shift()[0],a):t("Title 1.",a),i+=e(n.shift(),a),n.unshift("","");for(var d=2;d<n.length;d++){var c=n[d];if(i+="<tr>",d!=n.length-1){if(1==c.length){var f=10>d?"0"+d.toString()+"00":d.toString();i+=sprintf("<td id='%s' colspan='%s' class='%s %s'>",f,a.nCols,a.tdSubhead),i+=c[0]+"</td></tr>";continue}for(var u=0;u<c.length;u++){var p=10>d?"0"+d.toString():d.toString(),g=10>u?"0"+u.toString():u.toString();i+=sprintf("<td id='%s' class='%s'>%s</td>",p+g,a.tdAll,c[u])}}else for(var u=0;u<c.length;u++){var p=10>d?"0"+d.toString():d.toString(),g=10>u?"0"+u.toString():u.toString();i+=sprintf("<td id='%s' class='%s %s'>%s</td>",p+g,a.tdAll,a.trBreak,c[u])}i+="</tr>"}return i+="</table>",o},l.buildHtml=function(){return i="<table cellspacing=0 border=0>",i+=t(),i+=e(),i+=r(),i+=n(),i+="</table>"},l})},{}]},{},[1,2,3]);
+/*
+MODULE
+*/
+var gsatf = angular.module("gsaTableFormatter", ['ngRoute', 'xeditable']);
+
+/*
+options for xeditable
+
+gsatf.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
+*/
+
+/*
+CONTROLLER
+*/
+gsatf.controller("SimpleController", ['$scope', 'gsatfFactory', '$location',
+    function ($scope, gsatfFactory, $location) {
+        
+        $scope.table = gsatfFactory.getTable();
+        $scope.tableHtml = gsatfFactory.buildHtml();
+        
+        $scope.processData = function() {
+            $scope.table = gsatfFactory.extractData($scope.textarea);
+            $scope.tableHtml = gsatfFactory.buildHtml();
+            $location.path('view2');
+        };
+        
+        $scope.close = function() {
+            $('#submitDataBtn').popover('hide');
+        };
+        
+    }]);
+
+
+/* 
+ROUTE
+*/
+gsatf.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/view1.html',
+            controller: 'SimpleController'
+        })
+        .when('/view2', {
+            templateUrl: 'pages/view2.html',
+            controller: 'SimpleController'
+        })
+        .otherwise({ redirectTo: '/' });
+        
+}]);
+
+
+/* 
+# Define my custom directives
+
+[This site](http://subliminalsources.com/9/building-angularjs-bootstrap-components-popover-directive-part-1/)
+is a great reference
+
+## Basic ideas
+1. Create a module for directives, which will later be added to me main app module
+
+
+*/
+
+
+gsatf.directive('customPopover', ['$compile', 
+    function($compile){
+        return {
+            restrict: 'A',
+            //templateUrl: 'js/directives/templates/popover1.html',
+            //template: '<span>{{label}}</span>',
+
+            link: function (scope, el, attrs) {
+                //scope.label = attrs.popoverLabel;
+
+                $(el).popover({
+                    trigger: attrs.popoverTrigger,
+                    html: true,
+                    content: getContent(attrs.popoverHtml),
+                    placement: attrs.popoverPlacement
+                });
+                
+                function getContent(popoverHtml) {
+                    var options = {
+                        copyPaste:  $('#copy-paste_popover_content').html(),
+                        upload: $('#upload_popover_content').html(),
+                        whichOne: $('#which-one_popover_content').html()
+                    };
+                    return $compile(options[popoverHtml])(scope);
+                }       
+            }
+        };
+    }]);
+    
+    
+    
+gsatf.directive('dynamic', ['$compile', '$sce', function ($compile, $sce) {
+  return {
+    restrict: 'A',
+    replace: true,
+    link: function (scope, ele, attrs) {
+      scope.$watch(attrs.dynamic, function(html) {
+        ele.html(html);
+        $compile(ele.contents())(scope);
+        
+        
+        // I think I need to make the compiled output to be html?
+        //var content = $compile(ele.contents())(scope);
+        //return $sce.trustAsHtml(content);
+      });
+    }
+  };
+}]);
+/* This contains the data parser function
+This uses the sprintf.js found here https://github.com/alexei/sprintf.js*/
+
+
+/*
+FACTORY
+*/
+gsatf.factory("gsatfFactory", function() {
+    var factory = {};
+    var table = {
+        title: "",
+        headers: {
+            heads: [],
+            headDimensions: [],
+            subheads: [],
+            subheadDimensions: [] 
+        },
+        body: [],
+        footnotes: []
+    };
+    
+    var tableHtml = "";
+    
+    // declare css variables
+    var css = {
+        trTitle: "trTitle",
+        trBreak: "trBreak",
+        tdTitle: "tdTitle", //font-weight:bold; (apparently not in the specs)
+        center: "center",
+        left: "left",
+        right: "right",
+        tdSubhead: "tdSubhead",
+        tdBody: "tdBody",
+        borderTop: "borderTop",
+        nCols: 0, // updated in script
+        nRows: 1
+    };
+    
+    
+    // is this needed? eliminate?
+    factory.getTable = function() {
+        return table;
+    };
+    
+    
+    
+    /* extractData is passed the text from the textarea.
+    It parses this data and fills the variable `table` with the relevant parts,
+    so that buildHtml() can create the proper table from the correct data
+    */
+    factory.extractData = function(data) {
+        var isFilled = function(x) { return x !== ""; };
+        var isNotComment = function(x) { return (x[0] != "#" && x.slice(0,2) != "//"); };
+        var reFootnoteInc = /\*\d+/; //inclusive
+        var reFoodnoteExc = /\*(\d+)/; //exclusive
+        var reSubhead = /\[(.*)\]/;
+        var reDimension = /(\(.*\))/;
+        
+        // split each row into an array
+        data = data.split(/\n/);
+        
+        // clear empty rows at end and comments
+        data = data.filter(isFilled).filter(isNotComment);
+        
+        // split rows into cells & strip empty cells from "title rows" (rows where only the first cell is populated)
+        for (i = 0; i < data.length; i++) { 
+            data[i] = data[i].split(/\t/); 
+            data_filter = data[i].filter(isFilled);
+            // if there's only one non-empty cell AND its the first cell in the array
+            if (data_filter.length == 1 && data_filter[0] == data[i][0]) {
+                data[i] = data_filter;
+            }
+        }
+        
+        
+        // find max row length (= num of cols) - note: slice() effectively creates a copy of data, so the sorting doesn't affect the table
+        css.nCols = data.slice().sort(function (a, b) { return b.length - a.length; })[0].length;
+        
+        // Create Title (or empty placeholder)
+        // one element in first row indicates that its a built-in title
+        if (data[0].length == 1) {
+            table.title = data.shift()[0];
+            
+        // more than one element indicates that its the headers; less than one is likely an error
+        } else {
+            table.title += "Title 1."; 
+        }
+        
+        
+        
+        // Get headers row
+        var headers = data.shift();
+        
+        // split headers into the title and any corresponding dimensions
+        // todo: figure out why I need the "-1" in the `for` declaration - it seems hacky
+        for (var i = 0; i < headers.length - 1; i++) {
+            
+            // test the heading for subheads
+            if ( reSubhead.test(headers[i]) ) {
+                table.headers.heads[i] = headers[i].slice(0, headers[i].search(reSubhead));
+                table.headers.subheads[i] = headers[i].match(reSubhead)[1].split(",");
+                css.nRows = 2;
+                
+                // loop through the subheads and separate dimensions
+                for (var j = 0; j < table.headers.subheads[i].length; j++) {
+                    
+                    // if subhead contains reDimension
+                    if ( reDimension.test(table.headers.subheads[i][j]) ) {
+                        
+                        // create an array to hold subheadDimensions only if it doesn't exist
+                        if (table.headers.subheadDimensions[i] === undefined) {
+                            table.headers.subheadDimensions[i] = [];    
+                        } 
+                        
+                        // fill the subheadDimensions
+                        table.headers.subheadDimensions[i][j] = table.headers.subheads[i][j].match(reDimension)[0];
+                        table.headers.subheads[i][j] = table.headers.subheads[i][j].slice(0, table.headers.subheads[i][j].search(reDimension));
+                    }
+                }
+            } else {
+                
+                table.headers.heads[i] = headers[i];
+            }
+            
+            // test the heading for dimensions
+            if ( reDimension.test(headers[i]) ) {
+                table.headers.headDimensions[i] = table.headers.heads[i].match(reDimension)[0];
+                table.headers.heads[i] = table.headers.heads[i].slice(0, headers[i].search(reDimension));
+            }
+            
+        }
+        
+        
+        
+        // The remaining data is the body
+        table.body = data;
+        
+    } // function extractData
+    
+    
+    
+    
+    /* buildHtml returns a string of the html table
+    the contents are retrieved from the table variable
+    which is filled when the extractData function is called
+    */
+    factory.buildHtml = function() {
+        tableHtml = '<table cellspacing=0 border=0>';
+        tableHtml += getTitle();
+        tableHtml += getHeaders();
+        tableHtml += getBody();
+        tableHtml += getFootnotes();
+        tableHtml += "</table>";
+        return tableHtml;
+    } 
+
+
+
+    function getTitle() {
+        // set up html for first row
+        var head = sprintf("<tr><td id='0000' colspan='%(nCols)i' class='%(tdTitle)s %(center)s %(trTitle)s'><span editable-text='table.title'>", css);
+        var tail = "</span></td></tr>";
+        return head + "{{ table.title }}" + tail;
+    }
+
+
+
+    function getHeaders() {
+        
+        var reDimension = /\(.*\)/;
+        
+        // begin the header row    
+        var headers = "<tr>";
+        
+        // iterate through headings
+        for (var i = 0; i < table.headers.heads.length; i++) {
+            var id = i < 10 ? "010" + i.toString() : "01" + i.toString();
+            
+            var textAlign = i == 0 ? css.left : css.center;
+            
+            // if there are subheadings
+            if (table.headers.subheads[i] !== undefined) {
+                // no rowspan
+                headers += sprintf("<td colspan=%i id='%s' class='%s %s'>", table.headers.subheads[i].length, id, textAlign, css.trBreak);
+            } else {
+                // yes rowspan
+                headers += sprintf("<td rowspan=%i id='%s' class='%s %s'>", css.nRows, id, textAlign, css.trBreak);
+            }
+            
+            // add editable text into cell
+            headers += sprintf("<span editable-text='table.headers.heads[%i]'>{{ table.headers.heads[%i] }}</span>", i, i);
+            
+            // if there is a corresponding dimension for the header, print it on a separate line
+            if (table.headers.headDimensions[i] !== undefined ) {
+                headers += sprintf("<br /><span editable-text='table.headers.headDimensions[%i]'>{{ table.headers.headDimensions[%i] }}</span>", i, i);
+            }
+            
+                            
+            // if there are subheadings
+            if (table.headers.subheads[i] !== undefined) {
+                                
+                // add second row (subheading)
+                headers += "<tr>";
+                
+                // iterate through items in subheadings array
+                
+                for (var j = 0; j < table.headers.subheads[i].length; j++) {
+                    
+                    headers += sprintf("<td class='%(center)s %(trBreak)s'>", css);
+                    headers += sprintf("<span editable-text='table.headers.subheads[%i][%i]'>{{ table.headers.subheads[%i][%i] }}</span>", i, j, i, j);
+                    
+                    // if there is a corresponding dimension for the subheader, print it on a separate line
+                    if (table.headers.subheadDimensions[i] !== undefined ) {
+                        headers += sprintf("<br /><span editable-text='table.headers.subheadDimensions[%i][%i]'>{{ table.headers.subheadDimensions[%i][%i] }}</span>", i, j, i, j);
+                    }                    
+                }
+                
+
+                headers += "</tr>";    
+            } 
+            
+            // end the header cell
+            headers += "</td>";
+
+        }
+
+        return headers + "</tr>";
+        
+    }
+    
+    
+    
+    function getBody() {
+        
+        var bodyHtml = "";
+        
+        // iterate through remaining rows, and process
+        for (var i = 0; i < table.body.length; i++) {
+            
+            var row = table.body[i];
+            bodyHtml += "<tr>";
+            
+            // if there's only a single element in the row
+            if (table.body[i].length == 1) {
+                
+                var id = i < 10 ? "0" + i.toString() + "00" : i.toString() + "00";
+                bodyHtml += sprintf("<td id='%s' colspan=%s class='%s'><span editable-text='table.body[%s][0]'>", id, css.nCols, css.tdSubhead, i);
+                bodyHtml += sprintf("{{ table.body[%s][0] }}</span></td></tr>", i);
+                
+            } else {
+                
+                // loop through and process 
+                for (var j = 0; j < table.body[i].length; j++) {
+                    var textAlign = j == 0 ? css.left : css.center;
+                    
+                    var I = i < 10 ? "0"+i.toString() : i.toString();
+                    var J = j < 10 ? "0"+j.toString() : j.toString();
+                    bodyHtml += sprintf("<td id='%s' class='%s %s'><span editable-text='table.body[%i][%i]'>%s</span></td>", I+J, css.tdBody, textAlign, i, j, "{{ table.body[" + i + "][" + j + "] }}");
+                }
+            }
+                
+
+            bodyHtml += "</tr>";
+        }
+        
+        return bodyHtml;
+    }
+    
+    
+    
+    function getFootnotes() {
+        var html = sprintf("<tr><td colspan=%i class='%s'></td></tr>", css.nCols, css.borderTop);
+        
+        return html;
+    }
+    
+
+    
+    return factory;
+});
